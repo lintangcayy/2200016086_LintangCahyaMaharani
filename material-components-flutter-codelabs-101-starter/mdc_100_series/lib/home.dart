e2ddeeb9fcbca4b64ec7b2e76f081970a1307ec3
unknown <chylintang212@gmail.com>
2024-05-07 14:22:33 +0700

    prak6

--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
@@ -1,20 +1,6 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
+import 'supplemental/asymmetric_view.dart';
 import 'model/product.dart';
 import 'model/products_repository.dart';
 
@@ -37,6 +23,7 @@ class HomePage extends StatelessWidget {
     return Card(
       clipBehavior: Clip.antiAlias,
       // TODO: Adjust card heights (103)
+      elevation: 0.0,
       child: Column(
         // TODO: Center items on the card (103)
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -55,16 +42,19 @@ class HomePage extends StatelessWidget {
               padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
               child: Column(
                // TODO: Align labels to the bottom and center (103)
-               crossAxisAlignment: CrossAxisAlignment.start,
+               mainAxisAlignment: MainAxisAlignment.end,
+               crossAxisAlignment: CrossAxisAlignment.center,
                 // TODO: Change innermost Column (103)
                 children: <Widget>[
                  // TODO: Handle overflowing labels (103)
                  Text(
                     product.name,
                     style: theme.textTheme.titleLarge,
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
                     maxLines: 1,
                   ),
-                  const SizedBox(height: 8.0),
+                  const SizedBox(height: 4.0),
                   Text(
                     formatter.format(product.price),
                     style: theme.textTheme.titleSmall,
@@ -90,7 +80,6 @@ class HomePage extends StatelessWidget {
           'ON-SHOP',
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.blue,
         leading: IconButton(
           color: Colors.white,
           onPressed: (){
@@ -113,11 +102,8 @@ class HomePage extends StatelessWidget {
       
       
       // TODO: Add a grid view (102)
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(16.0),
-        childAspectRatio: 8.0/9.0,
-        children: _buildGridCards(context)
+      body: AsymmetricView(
+        products: ProductsRepository.loadProducts(Category.all),
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
@@ -1,18 +1,4 @@
-  // Copyright 2018-present the Flutter authors. All Rights Reserved.
-  //
-  // Licensed under the Apache License, Version 2.0 (the "License");
-  // you may not use this file except in compliance with the License.
-  // You may obtain a copy of the License at
-  //
-  // http://www.apache.org/licenses/LICENSE-2.0
-  //
-  // Unless required by applicable law or agreed to in writing, software
-  // distributed under the License is distributed on an "AS IS" BASIS,
-  // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  // See the License for the specific language governing permissions and
-  // limitations under the License.
-
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
   class LoginPage extends StatefulWidget {
     const LoginPage({Key? key}) : super(key: key);
@@ -25,6 +11,7 @@
     // TODO: Add text editing controllers (101)
     final _usernameController = TextEditingController();
     final _passwordController = TextEditingController();
+
     @override
     Widget build(BuildContext context) {
       return Scaffold(
@@ -37,44 +24,54 @@
                 children: <Widget>[
                   Image.asset('assets/diamond.png'),
                   const SizedBox(height: 16.0),
-                  const Text('SHRINE'),
+                  Text(
+                    'SHRINE',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                    ),
                 ],
               ),
               const SizedBox(height: 120.0),
+
               // TODO: Remove filled: true values (103)
-              // TODO: Add TextField widgets (101)
               TextField(
                 controller: _usernameController,
-    decoration: const InputDecoration(
-      filled: true,
-      labelText: 'Username',
-    ),
-  ),
-  // spacer
-  const SizedBox(height: 12.0),
-  // [Password]
-  TextField(
-    controller: _passwordController,
-    decoration: const InputDecoration(
-      filled: true,
-      labelText: 'Password',
-    ),
-    obscureText: true,
-  ),
+                decoration: const InputDecoration(
+                  //remove filled: true
+                  labelText: 'Username',
+                ),
+              ),
+
+              const SizedBox(height: 12.0),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  //remove filled: true
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              // TODO: Add TextField widgets (101)
+              
               // TODO: Add button bar (101)
               OverflowBar(
-    alignment: MainAxisAlignment.end,
-    // TODO: Add a beveled rectangular border to CANCEL (103)
-    children: <Widget>[
+                alignment: MainAxisAlignment.end,
+                // TODO: Add a beveled rectangular border to CANCEL (103)
+                children: <Widget>[
       // TODO: Add buttons (101)
-      TextButton(
-        child: const Text('CANCEL'),
-        onPressed: () {
-          // TODO: Clear the text fields (101)
-          _usernameController.clear();
-      _passwordController.clear();
-        },
-      ),
+                  TextButton(
+                    child: const Text('CANCEL'),
+                    onPressed: () {
+                      _usernameController.clear();
+                      _passwordController.clear();
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Theme.of(context).colorScheme.secondary,
+                      shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      ),
+                    ),
+                  ),
+                  
       // TODO: Add an elevation to NEXT (103)
       // TODO: Add a beveled rectangular border to NEXT (103)
       ElevatedButton(
@@ -83,6 +80,12 @@
       // TODO: Show the next page (101)
       Navigator.pop(context);
         },
+        style: ElevatedButton.styleFrom(
+          elevation: 8.0,
+          shape: const BeveledRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(7.0))
+          ),
+        ),
       ),
     ],
   ),
